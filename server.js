ba6cb9e63b4ae01ef0b247ad756b7ed682c04af7
Matthew Huntington <matt.huntington@gmail.com>
2016-06-29 15:56:59 +0000

    adding mongoose

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
 var port = process.env.PORT || 3000;
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/herokutest';
+
+var Person = mongoose.model('Person', {
+   name:String 
+});
 
 app.get('/', function(req, res){
-    res.send('works');
+    Person.create({name:'matt'}, function(){
+        res.send('works');
+    })
 })
 
-app.listen(port, function(){
+mongoose.connect(mongoDBURI);
+
+mongoose.connection.once('open', function(){
+    console.log('connected mongod');
+})
+
+app.listen(port, process.env.IP, function(){
     console.log('listening on port ' + process.env.PORT);
 })
